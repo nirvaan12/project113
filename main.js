654374cabcc086d79ed111dcf13249dd050661d0
nirvaan12 <85714636+nirvaan12@users.noreply.github.com>
2022-03-01 21:01:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload(){
+
+}
+
+
+function setup(){
+canvas = createCanvas(650,600);
+canvas.position(150,150);
+
+
+}
+
+function draw(){
+  fill("red");
+  stroke("red");
+  circle(100,100,100);
+circle(550,100,100);
+circle(100,500,100);
+circle(550,500,100);
+fill("green");
+stroke("green");
+rect(150,80,350,30);
+rect(150,480,350,30);
+rect(80,150,30,300);
+rect(530,150,30,300);
+}
